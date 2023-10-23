85c067405ab4c0dfdbbd11169e3aa5ea6a910a33
mARUwTC <chriswong0621@gmail.com>
2023-10-23 18:31:10 +0800

    update passive_information_gathering.py

--- pages/passive_information_gathering.py
+++ pages/passive_information_gathering.py
@@ -47,9 +47,9 @@ target = st.text_input('Enter IP address or URL')
 if st.button('Scan Vulnerabilities'):
     with st.spinner('Scanning...'):
         if platform.system() == "Linux" or platform.system() == "Darwin":
-            result = subprocess.run(f'cd nikto/program {operator()} ./nikto.pl -h {target}', capture_output=True, shell=True, text=True)
+            result = subprocess.run(f'./nikto/program/nikto.pl -h {target}', capture_output=True, shell=True, text=True)
         elif platform.system() == "Windows":
-            result = subprocess.run(f'attrib +x nikto\\program\\nikto.pl {operator()} perl nikto\\program\\nikto.pl -h {target}', capture_output=True, shell=True, text=True)
+            result = subprocess.run(f'attrib +x nikto\\program\\nikto.pl ; perl nikto\\program\\nikto.pl -h {target}', capture_output=True, shell=True, text=True)
         if result.stderr != '':
             st.code(result.stderr)
         else:
