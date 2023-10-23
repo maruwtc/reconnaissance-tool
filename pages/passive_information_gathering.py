2ba2ac8c48044d6709a043c320bfa5270b73b5b7
mARUwTC <chriswong0621@gmail.com>
2023-10-23 18:11:45 +0800

    update passive_information_gathering.py

--- pages/passive_information_gathering.py
+++ pages/passive_information_gathering.py
@@ -3,8 +3,47 @@ from subprocess import run
 from osoperator import operator
 import platform
 import subprocess
+import whois
+import pandas as pd
+import dnsrecon
 
-st.header('HTTP')
+st.subheader('Whois')
+whois_url = st.text_input('Enter web address')
+show_results = st.empty()
+if st.button('Start'):
+    whois_data = whois.whois(whois_url)
+    st.write("Domain Name: "+whois_data.get("domain_name", None))
+    st.write("Registrar: "+whois_data.get("registrar", None))
+    st.write("Whois Server: "+whois_data.get("whois_server", None))
+    updated_date = whois_data.get("updated_date", None)
+    if isinstance(updated_date, list):
+        updated_date = ", ".join(str(date) for date in updated_date)
+    st.write("Updated Date: " + str(updated_date))
+    st.write("Creation Date: " + str(whois_data.get("creation_date", None)))
+    st.write("Expiration Date: " + str(whois_data.get("expiration_date", None)))
+    name_servers = whois_data.get("name_servers", None)
+    if name_servers:
+        lowercase_name_servers = [server.lower() for server in name_servers]
+        unique_name_servers = sorted(list(set(lowercase_name_servers)))
+        name_servers_df = pd.DataFrame(unique_name_servers, columns=["Name Servers"])
+        st.dataframe(name_servers_df, hide_index=True)
+    else:
+        st.write("No name servers found.")
+
+    st.subheader('DNS records')
+
+st.divider()
+
+if st.button('DNS Reconn'):
+    result = subprocess.run(f'dnsrecon -d {whois_url}', capture_output=True, shell=True, text=True)
+    if result.stderr != '':
+        st.code(result.stderr)
+    else:
+        st.code(result.stdout)
+
+st.divider()
+
+st.subheader('HTTP Vulnerability Scan')
 
 target = st.text_input('Enter IP address or URL')
 
